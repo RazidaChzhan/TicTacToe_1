9c03eb0843b1b4461fe6344fe4ed3537445a3ba4
RazidaChzhan <chrazida@gmail.com>
2019-04-10 22:54:51 +0600

    Второе задание

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 var button = document.querySelectorAll("button");
+var z = 0; //0 - cross 1 - zero
 
-function myFunc() {
+for (var i = 0; i < button.length; i++) {
+  button[i].addEventListener("click", myFunction());
+}
+
+function myFunction() {
+  if (z === 0) {
     this.style.backgroundImage = "url(images/cross.png)";
     this.style.backgroundSize ="cover";
-}
-for (var i = 0; i < button.length; i ++) {
-    button[i].addEventListener("click", myFunc);    
+    z = 1;
   }
+  if (z === 1) {
+    this.style.backgroundImage = "url(images/zero.png)";
+    this.style.backgroundSize ="cover";
+    z = 0;
+  }
+}
