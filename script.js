7ccecef942d937d6aab1144bb4b47c4e8c49f965
RazidaChzhan <chrazida@gmail.com>
2019-04-13 21:38:18 +0600

    Реализована блокировка повторного клика на кнопку

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 var button = document.querySelectorAll("button");
 var z = 0; //0 - cross 1 - zero
+var values = [];
 
 for (var i = 0; i < button.length; i++) {
-  button[i].addEventListener("click", myFunction);
-}
-
+      button[i].addEventListener("click", myFunction);
+  }
+ 
 function myFunction() {
+  var num = +this.getAttribute("btn-num");
+  // console.log ('btn-num = ' + num);
   if (z === 0) {
-    this.style.backgroundImage = "url(images/cross.png)";
-    this.style.backgroundSize ="cover";
-    z = 1;
+    if (values [num] === 1 || values[num] === 0) {
+      return;
+    } else {
+      this.style.backgroundImage = "url(images/cross.png)";
+      this.style.backgroundSize ="cover";
+      z = 1;
+      values [num] = z;  
+    }
   }
+
   else if (z === 1) {
-    this.style.backgroundImage = "url(images/zero.png)";
-    this.style.backgroundSize ="cover";
-    z = 0;
+    if (values [num] === 1 || values[num] === 0) {
+      return;
+    } else {
+      this.style.backgroundImage = "url(images/zero.png)";
+      this.style.backgroundSize ="cover";
+      z = 0;
+      values [num] = z;  
+    }
   }
+  // console.log (values);
 }
