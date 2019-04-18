da32ac7d7f35d5a4e18f743961089bf5574db744
RazidaChzhan <chrazida@gmail.com>
2019-04-18 21:32:32 +0600

    Добавлена проверка выигрышных комбинаций и отметка чей ход

--- script.js
+++ script.js
@@ -1,26 +1,67 @@
 var button = document.querySelectorAll("button");
 var z = 0; //0 - cross 1 - zero
 var values = [];
+var currentPlayer = 'Полина';
+var winCombinations = [
+[0, 1, 2, 3],
+[4, 5, 6, 7],
+[8, 9, 10, 11],
+[12, 13, 14, 15],
+[0, 4, 8, 12],
+[1, 5, 9, 13],
+[2, 6, 10, 14],
+[3, 7, 11, 15],
+[0, 5, 10, 15],
+[3, 6, 9, 12]
+];
+var stepCount = 0;
+var dataX = [], 
+    dataO = [];
+    
 
 for (var i = 0; i < button.length; i++) {
-      button[i].addEventListener("click", myFunction);
-  }
- 
+  button[i].addEventListener("click", myFunction);
+}
+
 function myFunction() {
   var num = +this.getAttribute("btn-num");
-  // console.log ('btn-num = ' + num);
   if (z === 0) {
+      currentPlayer = 'Дарья';
+      message.innerText = "Ваш ход, " +  currentPlayer;
+    if (stepCount === 15) {
+        message.innerText = "Предлагаем начать игру заново, ничья";
+    } else {
+      stepCount ++;
+    }
+
     if (values [num] === 1 || values[num] === 0) {
-      return;
+        return;
     } else {
       this.style.backgroundImage = "url(images/cross.png)";
       this.style.backgroundSize ="cover";
       z = 1;
       values [num] = z;  
+      dataX.push(num);
+
+      console.log ('dataX ' + dataX);
+      
+      if (dataX.length > 3){
+        console.log ('checkWin: ' + checkWin(dataX, num));
+        if (checkWin(dataX, num)){
+          message.innerText = "Ура! Полина победила!";
+        }
+      }
     }
   }
 
   else if (z === 1) {
+    currentPlayer = 'Полина';
+    message.innerText = "Ваш ход,  " + currentPlayer;
+    if (stepCount === 15) {
+      message.innerText = "Предлагаем начать игру заново, ничья";
+    } else {
+      stepCount ++;
+    }
     if (values [num] === 1 || values[num] === 0) {
       return;
     } else {
@@ -28,7 +69,40 @@ function myFunction() {
       this.style.backgroundSize ="cover";
       z = 0;
       values [num] = z;  
+      dataO.push(num);
+      // console.log ('dataO ' + dataO);
+      if (dataO.length > 3){
+        // console.log ('checkWin: ' + checkWin(dataO, num));
+        if (checkWin(dataO, num)){
+          message.innerText = "Ура! Дарья победила!";
+
+        } 
+      }
     }
   }
-  // console.log (values);
+}
+
+function checkWin(arr, number) {
+  for (var w = 0, wLen = winCombinations.length; w < wLen; w++) {
+    var someWinArr = winCombinations[w],
+    count = 0;
+    if (someWinArr.indexOf(number) !== -1) {
+      for (var k = 0, kLen = someWinArr.length; k < kLen; k++) {
+        if (arr.indexOf(someWinArr[k]) !== -1) {
+          count++;
+          if (count === 4) {
+            return true;
+          }
+        }   
+      }
+      count = 0;
+    }
+  }
+}
+
+function resetDefault(){
+  dataX = []; 
+  dataO = [];
+  values = [];
+
 }
