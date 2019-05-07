2f7fbdca24b4273c582871c74a30fe52652efb5b
RazidaChzhan <chrazida@gmail.com>
2019-05-07 23:48:29 +0600

    функция отправки сообщений подключена, но сервер отвечает, что вы не можете отправлять сообщения

--- idRoom.js
+++ idRoom.js
@@ -1,34 +1,87 @@
+// Model
+var roomId,
+	dataRoomInit,
+	dataGameInfo, 
+	dataUpDateField, 
+	dataSwitchCurrentPlayer, 
+	dataGameStart,
+	socket = io('ws://localhost:3001');
+
 var idRoomBtn = document.getElementById('idRoom');
-var idRoomMsg = document.getElementById('roomId');
+var idRoomMsg = document.getElementById('message');
 var idRoomTxt = document.getElementById('roomIdTxt');
 var idRoomBtnToServer = document.getElementById('roomIdBtn');
+var idRoomBtn = document.getElementById('idRoom');
+
+var receiveвMsg = document.getElementById('messageId');
+var messageTxt = document.getElementById('messageIdTxt');
+var messageBtn = document.getElementById('messageIdBtn');
 
 
+//Controller
+
+if (socket != null){
+	console.log(`Server Socket: ${socket}`);
+} else {
+	alert ('Подключитесь к серверу!');
+}
+	// Подписка на сервисы сервера
+socket.on ('action', (data) => { 
+		switch(data.type){
+		case 'roomInit': dataRoomInit = data;
+		console.log('server data dataRoomInit', dataRoomInit);
+		break;
+		case 'gameInfo': dataGameInfo = data;
+		console.log('server data dataGameInfo', dataGameInfo);
+		break;
+		case 'updateField': dataUpDateField = data;
+		console.log('server data dataUpdateField', dataUpDateField);
+		break;
+		case 'switchCurrentPlayer': dataSwitchCurrentPlayer = data;
+		console.log('server data dataSwitchCurrentPlayer', dataSwitchCurrentPlayer);
+		break;
+		case 'gameStart': dataGameStart = data;
+		console.log('server data dataGameStart', dataGameStart);
+		break;
+	}	
+});
+
+	// Listeners
 idRoomBtn.addEventListener("click", getRoomId);
-idRoomBtnToServer.addEventListener("click", sentRoomIdToServer);
+idRoomBtnToServer.addEventListener("click", connectToRoom);
+messageBtn.addEventListener("click", messageToRoom);
 
+
+// Functions
 function getRoomId () {
-	var roomId;
-	var socket = io('ws://localhost:3001');
-	console.log(socket);
-	
-	socket.on('action', (data) => { 
-	  console.log('server data', data);
-	  roomId = data.data.id;
-      console.log(`id room: ${roomId}`);
-      	idRoomMsg.innerText = "Ваш номер комнаты: " + roomId;
+	roomId = dataRoomInit.data.id;
+	idRoomMsg.innerText = "Ваш номер комнаты: " + roomId;
+}
+
+function connectToRoom () {
+	var room = idRoomTxt.value;
+	socket.emit('action', {       
+		type: 'connectToRoom',
+		data: {
+			roomId: room,
+			cb: function(data) {
+				console.log(data);
+			}
+		}	
 	});
 }
 
-function sentRoomIdToServer () {
-	var socket = io('ws://localhost:3001');
-	var room = idRoomTxt.value;
-	console.log (room);
-		socket.emit('action', {       
-			type: 'connectToRoom',
-			data: {
-				roomId: room,
-				cb: function(data) {
+function messageToRoom () {
+	//var message = messageTxt.value;
+	roomId = dataRoomInit.data.id;
+	console.log('Send message');
+	console.log('Send.message.roomid', roomId);
+	socket.emit('action', {       
+		type: 'message',
+		data: {
+			roomId: roomId,
+			message: 'Tic-Tac-Toe',
+			cb: function(data) {
 				console.log(data);
 			}
 		}
